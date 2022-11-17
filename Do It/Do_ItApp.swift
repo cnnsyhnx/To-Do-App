a07e71360084bbf884b40fcff4415579fe8fa389
Can Seyhan <cnnsyhnx@gmail.com>
2022-11-17 17:05:36 +0100

    Initial Commit

---
+++ Do It/Do_ItApp.swift
@@ -0,0 +1,17 @@
+//
+//  Do_ItApp.swift
+//  Do It
+//
+//  Created by Can Seyhan on 17/11/2022.
+//
+
+import SwiftUI
+
+@main
+struct Do_ItApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
